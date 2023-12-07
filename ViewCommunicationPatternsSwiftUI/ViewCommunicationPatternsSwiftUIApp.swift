ed1377f2c720676fa7852a199751f36fe253c378
Ryan Willis <ryan_h_willis@hotmail.com>
2023-12-07 10:46:19 +0000

    Initial Commit

---
+++ ViewCommunicationPatternsSwiftUI/ViewCommunicationPatternsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewCommunicationPatternsSwiftUIApp.swift
+//  ViewCommunicationPatternsSwiftUI
+//
+//  Created by Ryan Hayd'n Willis on 07/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct ViewCommunicationPatternsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
